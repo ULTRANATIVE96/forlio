9152078573710e6550dd3dfc6c5c7fd1b7e49e3a
Nhlayiseko Divine <hlayisekohh7@gmail.com>
2025-12-22 06:31:56 +0200

    Fix capitalization of Utils imports for Vercel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import {cn} from "@/lib/utils"
+import {cn} from "@/lib/Utils"
 import { useEffect, useState } from "react"
 import { Menu, X } from "lucide-react"
 
--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import {cn} from '@/lib/utils'
+import {cn} from '@/lib/Utils'
 const skills=[
     //frontend
     {name:"HTML/CSS", level:97, category:"frontend"},
